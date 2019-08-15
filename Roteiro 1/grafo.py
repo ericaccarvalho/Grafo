77fd35975b7543612e8e1890c46003221e466b86
ericaccarvalho <31082192+ericaccarvalho@users.noreply.github.com>
2019-08-15 10:42:09 -0300

    Add files via upload

---
+++ Roteiro 1/grafo.py
@@ -0,0 +1,244 @@
+class VerticeInvalidoException(Exception):
+    pass
+
+class ArestaInvalidaException(Exception):
+    pass
+
+class Grafo:
+
+    QTDE_MAX_SEPARADOR = 1
+    SEPARADOR_ARESTA = '-'
+
+    def __init__(self, N=[], A={}):
+        '''
+        Constrói um objeto do tipo Grafo. Se nenhum parâmetro for passado, cria um Grafo vazio.
+        Se houver alguma aresta ou algum vértice inválido, uma exceção é lançada.
+        :param N: Uma lista dos vértices (ou nodos) do grafo.
+        :param V: Uma dicionário que guarda as arestas do grafo. A chave representa o nome da aresta e o valor é uma string que contém dois vértices separados por um traço.
+        '''
+        for v in N:
+            if not(Grafo.verticeValido(v)):
+                raise VerticeInvalidoException('O vértice ' + v + ' é inválido')
+
+        self.N = N
+
+        for a in A:
+            if not(self.arestaValida(A[a])):
+                raise ArestaInvalidaException('A aresta ' + A[a] + ' é inválida')
+
+        self.A = A
+
+    def arestaValida(self, aresta=''):
+        '''
+        Verifica se uma aresta passada como parâmetro está dentro do padrão estabelecido.
+        Uma aresta é representada por um string com o formato a-b, onde:
+        a é um substring de aresta que é o nome de um vértice adjacente à aresta.
+        - é um caractere separador. Uma aresta só pode ter um único caractere como esse.
+        b é um substring de aresta que é o nome do outro vértice adjacente à aresta.
+        Além disso, uma aresta só é válida se conectar dois vértices existentes no grafo.
+        :param aresta: A aresta que se quer verificar se está no formato correto.
+        :return: Um valor booleano que indica se a aresta está no formato correto.
+        '''
+
+        # Não pode haver mais de um caractere separador
+        if aresta.count(Grafo.SEPARADOR_ARESTA) != Grafo.QTDE_MAX_SEPARADOR:
+            return False
+
+        # Índice do elemento separador
+        i_traco = aresta.index(Grafo.SEPARADOR_ARESTA)
+
+        # O caractere separador não pode ser o primeiro ou o último caractere da aresta
+        if i_traco == 0 or aresta[-1] == Grafo.SEPARADOR_ARESTA:
+            return False
+
+        # Verifica se as arestas antes de depois do elemento separador existem no Grafo
+        if not(self.existeVertice(aresta[:i_traco])) or not(self.existeVertice(aresta[i_traco+1:])):
+            return False
+
+        return True
+
+    @classmethod
+    def verticeValido(self, vertice=''):
+        '''
+        Verifica se um vértice passado como parâmetro está dentro do padrão estabelecido.
+        Um vértice é um string qualquer que não pode ser vazio e nem conter o caractere separador.
+        :param vertice: Um string que representa o vértice a ser analisado.
+        :return: Um valor booleano que indica se o vértice está no formato correto.
+        '''
+        return vertice != '' and vertice.count(Grafo.SEPARADOR_ARESTA) == 0
+
+    def existeVertice(self, vertice=''):
+        '''
+        Verifica se um vértice passado como parâmetro pertence ao grafo.
+        :param vertice: O vértice que deve ser verificado.
+        :return: Um valor booleano que indica se o vértice existe no grafo.
+        '''
+        return Grafo.verticeValido(vertice) and self.N.count(vertice) > 0
+
+    def existeAresta(self, aresta=''):
+        '''
+        Verifica se uma aresta passada como parâmetro pertence ao grafo.
+        :param aresta: A aresta a ser verificada
+        :return: Um valor booleano que indica se a aresta existe no grafo.
+        '''
+        existe = False
+        if Grafo.arestaValida(self, aresta):
+            for k in self.A:
+                if aresta == self.A[k]:
+                    existe = True
+
+        return existe
+
+    def adicionaVertice(self, v):
+        '''
+        Adiciona um vértice no Grafo caso o vértice seja válido e não exista outro vértice com o mesmo nome
+        :param v: O vértice a ser adicionado
+        :raises: VerticeInvalidoException se o vértice passado como parâmetro não puder ser adicionado
+        '''
+        if self.verticeValido(v) and not self.existeVertice(v):
+            self.N.append(v)
+        else:
+            raise VerticeInvalidoException('O vértice ' + v + ' é inválido')
+
+    def adicionaAresta(self, nome, a):
+        '''
+        Adiciona uma aresta no Grafo caso a aresta seja válida e não exista outra aresta com o mesmo nome
+        :param v: A aresta a ser adicionada
+        :raises: ArestaInvalidaException se a aresta passada como parâmetro não puder ser adicionada
+        '''
+        if self.arestaValida(a):
+            self.A[nome] = a
+        else:
+            ArestaInvalidaException('A aresta ' + self.A[a] + ' é inválida')
+
+    def vertices_nao_adjacentes(self):
+        lista_arestas = self.A.values()
+        resultado=[]
+        for i in self.N:
+            for j in self.N:
+                aresta_indo='{}-{}'.format(i, j)
+                aresta_vindo = '{}-{}'.format(j, i)
+                if (aresta_indo not in lista_arestas) and (aresta_vindo not in lista_arestas):
+                    resultado.append(aresta_indo)
+        return resultado
+
+    def ha_laco(self):
+        lista_arestas=self.A.values()
+        for i in lista_arestas:
+            v1,v2=i.split(self.SEPARADOR_ARESTA)
+            if v1==v2:
+                return True
+        return False
+
+    def ha_paralelas(self):
+
+        lista_arestas = list(self.A.values())
+
+        for i in lista_arestas:
+            if lista_arestas.count(i) > 1:
+                return True
+        return  False
+
+    def grau(self, vertice):
+
+        lista_arestas = self.A
+
+        cont = 0
+
+        for i in lista_arestas:
+            print(i)
+            if (lista_arestas[i][0] == vertice or lista_arestas[i][2] == vertice):
+                cont += 1
+
+        return cont
+
+
+
+    def arestas_sobre_vertice(self,vertice):
+
+        lista_arestas = self.A
+        arestas = []
+
+        for i in lista_arestas:
+            if (lista_arestas[i][0] == vertice or lista_arestas[i][2] == vertice):
+                arestas.append(i)
+
+        return arestas
+
+    def eh_completo(self):
+
+        verticies = self.N
+
+        lista_arestas = list(self.A.values())
+        lista_grafo_completo =[]
+
+        if len(verticies) <= 2:
+            return True
+
+        for i in range(len(verticies)):
+            for j in range(i+1, len(verticies)):
+                lista_grafo_completo.append("{}{}{}".format(verticies[i],self.SEPARADOR_ARESTA,{verticies[j]}))
+
+
+        if len(lista_grafo_completo) == len(lista_arestas):
+
+            return True
+
+
+        else:
+            return False
+
+
+
+    def __str__(self):
+        '''
+        Fornece uma representação do tipo String do grafo.
+        O String contém um sequência dos vértices separados por vírgula, seguido de uma sequência das arestas no formato padrão.
+        :return: Uma string que representa o grafo
+        '''
+        grafo_str = ''
+
+        for v in range(len(self.N)):
+            grafo_str += self.N[v]
+            if v < (len(self.N) - 1):  # Só coloca a vírgula se não for o último vértice
+                grafo_str += ", "
+
+        grafo_str += '\n'
+
+        for i, a in enumerate(self.A):
+            grafo_str += self.A[a]
+            if not(i == len(self.A) - 1): # Só coloca a vírgula se não for a última aresta
+                grafo_str += ", "
+
+        return grafo_str
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
